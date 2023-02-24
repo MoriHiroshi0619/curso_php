c019d2abb3cb8add3efc62c1549f1c13eb04b090
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-02-24 16:57:02 -0400

    updated

---
+++ modulo01/testes/t005.php
@@ -0,0 +1,12 @@
+<?php 
+    $nome = "Hirohsi";
+    $sobrenome = "mori";
+    $ano = date('Y');
+
+    echo <<< teste
+        olá galera! meu nome é $nome.
+        sobrenome é $sobrenome.
+            Como está sendo esse ano de $ano.
+        :)\u{1f596}\n
+    teste;
+?>
