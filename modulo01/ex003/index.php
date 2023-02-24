4c6f9dda00040c7823e3e5243b8725599531b93b
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-02-24 01:52:29 -0400

    updated

---
+++ modulo01/ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+    <style>
+        p{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>isso é um teste</h1>
+    <?php 
+        $nome = "hiroshi";
+        echo "<p>meu nome é $nome</p>"
+    ?>
+</body>
+</html>
