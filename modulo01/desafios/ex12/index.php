a7a14f59f02d91c7c4ac6d0604e8b3d2ae948468
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-26 19:46:33 -0400

    Update index.php

--- modulo01/desafios/ex12/index.php
+++ modulo01/desafios/ex12/index.php
@@ -9,26 +9,28 @@
 </head>
 <body>
     <?php 
-        
-    
+        $halibi = 0;
+        $total_segundos = $_REQUEST['seg'] ?? 0;
+        $total_minutos = $total_segundos % 60;
+
     ?>
     <main>
         <h1>Calculadora de tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="seg">Qual é o total de segundos?</label>
-            <input type="number" id="seg" name="seg">
+            <input type="number" id="seg" name="seg" value="0">
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <h2>Totalizando Tudo</h2>
-        <p>Analisando o valor que você digitou, <strong><?=?> segundos</strong> equivalem a um total de:</p>
+        <p>Analisando o valor que você digitou, <strong><?=$total_segundos?> segundos</strong> equivalem a um total de:</p>
         <ul>
-            <li><?=?> semanas</li>
-            <li><?=?> dias</li>
-            <li><?=?> horas</li>
-            <li><?=?> minutos</li>
-            <li><?=?> segundos</li>
+            <li><?=$halibi?> semanas</li>
+            <li><?=$halibi?> dias</li>
+            <li><?=$halibi?> horas</li>
+            <li><?=$total_minutos?> minutos</li>
+            <li><?=$total_segundos?> segundos</li>
         </ul>
     </section>
 </body>
