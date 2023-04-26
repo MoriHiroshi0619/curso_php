82303c24f53624239bb50ff70dd24a8845b20d75
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-25 21:01:42 -0400

    Create index.php

---
+++ modulo01/desafios/ex12/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador do tempo</title>
+    <link rel="stylesheet" href="../../style/style.css">
+</head>
+<body>
+    <main>
+        <h1>Analisador de tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <label for="seg"></label>
+        </form>
+    </main>
+</body>
+</html>
