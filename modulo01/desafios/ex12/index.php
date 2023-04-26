96453abd0df9176ea1f1dd26d684b203ba641a89
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-25 21:54:52 -0400

    Update index.php

--- modulo01/desafios/ex12/index.php
+++ modulo01/desafios/ex12/index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Analisador do tempo</title>
+    <title>Calculadora do tempo</title>
     <link rel="stylesheet" href="../../style/style.css">
 </head>
 <body>
     <main>
-        <h1>Analisador de tempo</h1>
+        <h1>Calculadora de tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-            <label for="seg"></label>
+            <label for="seg">Qual é o total de segundos?</label>
+            <input type="number" id="seg" name="seg">
+            <input type="submit" value="Calcular">
         </form>
     </main>
 </body>
